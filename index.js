360ad3d15a4bbcb08b4ecdf66c9da229f4401218
PaolaCorreaa <95337056+PaolaCorreaa@users.noreply.github.com>
2021-12-08 01:42:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-Paola Correa Gómez
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,6 +9,7 @@ const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = require("express")();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -39,3 +39,6 @@ showTimes = () => {
   }
   return result;
 }
+app.get("", (req,res)>=>{ res.send("Paola Correa");});
+app.listen(PORT, () => {
+  console.log('App up at port ${PORT}'); });
